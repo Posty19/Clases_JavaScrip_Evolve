7630130e2fb5077ff87423b700cd3e44c7c40380
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-13 19:17:50 +0100

    moduralizacion, necesita rrepaso

---
+++ 25-02-13_Modularizacion_Arrrays_Objetos_Ejs/Teoria/script/calculadora.js
@@ -0,0 +1,12 @@
+export function suma(n1, n2){
+    return n1+n2;
+}
+export function resta(n1, n2){
+    return n1-n2;
+}
+export function multiplicar(n1, n2){
+    return n1*n2;
+}
+export function dividir(n1, n2){
+    return n1/n2;
+}
---
+++ 25-02-13_Modularizacion_Arrrays_Objetos_Ejs/Teoria/script/script.js
@@ -0,0 +1,9 @@
+import calculadora from 'calculadora.js';
+
+const n1 = 10;
+const n2 = 5;
+
+console.log(calculadora.suma(n1,n2));
+console.log(calculadora.resta(n1,n2));
+console.log(calculadora.multiplicar(n1,n2));
+console.log(calculadora.dividir(n1,n2));
