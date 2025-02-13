b676976dec23ef28ddc371ed21db5d7261ee1d20
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-13 19:48:59 +0100

    destructurin

--- 25-02-13_Modularizacion_Arrrays_Objetos_Ejs/Teoria/script/calculadora.js
+++ 25-02-13_Modularizacion_Arrrays_Objetos_Ejs/Teoria/script/calculadora.js
@@ -1,12 +1,14 @@
-export function suma(n1, n2){
+function suma(n1, n2){
     return n1+n2;
 }
-export function resta(n1, n2){
+function resta(n1, n2){
     return n1-n2;
 }
-export function multiplicar(n1, n2){
+function multiplicar(n1, n2){
     return n1*n2;
 }
-export function dividir(n1, n2){
+function dividir(n1, n2){
     return n1/n2;
 }
+
+module.exports(suma,resta,multiplicar,dividir)
--- 25-02-13_Modularizacion_Arrrays_Objetos_Ejs/Teoria/script/script.js
+++ 25-02-13_Modularizacion_Arrrays_Objetos_Ejs/Teoria/script/script.js
@@ -7,3 +7,27 @@ console.log(calculadora.suma(n1,n2));
 console.log(calculadora.resta(n1,n2));
 console.log(calculadora.multiplicar(n1,n2));
 console.log(calculadora.dividir(n1,n2));
+
+//DESTRUCTURIN
+const coche = {
+    color:'rojo',
+    modelo:'mustang',
+    marca:'ford',
+    anno:1969
+}
+
+const {color:colorCoche,modelo:modeloCoche} = coche;
+
+console.log(colorCoche);
+console.log(modeloCoche);
+
+const nums = [3,6,5,4,3,1];
+
+const [prim, , terc] = nums;
+
+console.log(prim);
+console.log(terc);
+
+const [ , , ...resto] = nums;
+
+console.log(resto);
