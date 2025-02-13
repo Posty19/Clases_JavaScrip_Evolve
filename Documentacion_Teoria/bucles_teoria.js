370d5b68a142333d90b474acf0e05223c33b7866
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-13 17:13:30 +0100

    creacion de carpeta de documentacion y teoria y creacion del archibo de bucles

---
+++ Documentacion_Teoria/bucles_teoria.js
@@ -0,0 +1,30 @@
+// Bucles
+
+//-------------------------------- FOR
+for (let i = 0; i < 9; i++) {
+  //console.log(`vuelta del buqule: ${i+1}`)
+}
+
+let n1 = 5;
+let n2 = 5;
+console.log(n1++);
+console.log(++n2);
+
+let n3 = 5;
+n3 = n3 += 3;
+console.log(n3);
+
+//-------------------------------- While
+let j = 1;
+while (j < 15) {
+  console.log(`bucle while ${j}`);
+  j++;
+}
+
+//-------------------------------- DO WHILE
+let k = 16;
+
+do {
+  console.log(`bucle doWhile ${k}`);
+  k++;
+} while (k < 15);
