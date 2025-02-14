ee793b0eacdcfba45a9f255c93ca38ae2de0cb3d
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-14 10:22:19 +0100

    ejercicio 1 Ejerccios avanzados Js 25/13/25

--- 25-02-13_Modularizacion_Arrrays_Objetos_Ejs/Ejercicios/script/script.js
+++ 25-02-13_Modularizacion_Arrrays_Objetos_Ejs/Ejercicios/script/script.js
@@ -18,16 +18,24 @@ const addProduct = (name,quantity,price)=>{
 
 //eliminar producto
 const delProduct = ()=>{
-
+  inventario.pop();
 }
 
 //filtrar producto por precio mayor al dado
-const selectProductByPrice = (price)=>{
-
+const  selectProductByPrice= (price)=>{
+  const filtrados = inventario.filter(el => el.precio>price);
+  return filtrados;
 }
 //ordenar productos por cantidad mayor a menor
 const orderProductQuantity = ()=>{
-
+  inventario.sort((a,b)=>{
+    return b.cantidad - a.cantidad;
+  })
 }
+addProduct('pantalla',45,124);
+console.log(selectProductByPrice(50));
+orderProductQuantity()
+console.log(inventario);
 
 //2️⃣ Análisis de Notas de Estudiantes
+
