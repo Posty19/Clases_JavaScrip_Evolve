03f3be9f754c1f1ff606d66bd9cf960fa8364350
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-18 12:16:55 +0100

    Ej5 Modulacion (terminado) 14/02/25

--- 25-02-14_Modularizacion/Ejercicios/Ej5(Alumnos_Notas)/index.js
+++ 25-02-14_Modularizacion/Ejercicios/Ej5(Alumnos_Notas)/index.js
@@ -1,18 +1,29 @@
 const {
-    mostrarAlumnos,
-    agregarAlumnos,
-    mediaNotas,
-    alumnosAprobados
-}= require('./registro.js');
+  mostrarAlumnos,
+  agregarAlumnos,
+  mediaNotas,
+  alumnosAprobados,
+  ordenarAlumnos,
+  buscarAlumno,
+} = require("./registro.js");
 
+agregarAlumnos("Daniel", [6, 5, 9, 4, 8, 3, 7]);
+agregarAlumnos("Paco", [2, 10, 9, 8, 6, 7]);
+agregarAlumnos("Lucia", [5, 1, 9, 3, 5]);
+agregarAlumnos("Andrea", [8, 3, 7]);
 
-agregarAlumnos('Daniel',[6,5,9,4,8,3,7]);
-agregarAlumnos('Paco',[2,10,9,8,6,7]);
-agregarAlumnos('Lucia',[5,1,9,3,5]);
-agregarAlumnos('Andrea',[8,3,7]);
+console.log("Alumnos");
+mostrarAlumnos();
 
-console.log(mostrarAlumnos());
-
+console.log("lista media de notas");
 console.log(mediaNotas());
 
+console.log("Alum,nos aprobados: ");
 console.log(alumnosAprobados());
+
+console.log("ordenar alumnos:");
+ordenarAlumnos();
+mostrarAlumnos();
+
+console.log("Buscar a Paco:");
+console.log(buscarAlumno("Paco"));
--- 25-02-14_Modularizacion/Ejercicios/Ej5(Alumnos_Notas)/registro.js
+++ 25-02-14_Modularizacion/Ejercicios/Ej5(Alumnos_Notas)/registro.js
@@ -9,13 +9,13 @@ const agregarAlumnos = (nombre, notas) => alumnos.push({ nombre, notas });
 //✅ Calcular el promedio de cada alumno (map).
 
 const mediaNotas = () => {
-  return alumnos.map(el=>mediaAlumno(el));
+  return alumnos.map((el) => mediaAlumno(el));
 };
 
 const mediaAlumno = (alumno) => {
   let media = 0;
   alumno.notas.forEach((el) => {
-    media+=el;
+    media += el;
   });
   media /= alumno.notas.length;
   return media;
@@ -23,14 +23,22 @@ const mediaAlumno = (alumno) => {
 
 //✅ Filtrar alumnos aprobados (filter).
 
-const alumnosAprobados = () => alumnos.filter(el => mediaAlumno(el)>5);
+const alumnosAprobados = () => alumnos.filter((el) => mediaAlumno(el) > 5);
 
 //✅ Ordenar alumnos por promedio (sort).
+
+const ordenarAlumnos = () =>
+  alumnos.sort((a, b) => mediaAlumno(b) - mediaAlumno(a));
+
 //✅ Buscar un alumno por nombre (find).
 
+const buscarAlumno = (name) => alumnos.find((el) => el.nombre === name);
+
 module.exports = {
-    mostrarAlumnos,
+  mostrarAlumnos,
   agregarAlumnos,
   mediaNotas,
-  alumnosAprobados
+  alumnosAprobados,
+  ordenarAlumnos,
+  buscarAlumno,
 };
