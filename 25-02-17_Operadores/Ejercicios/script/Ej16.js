d1826142843df3c89659a33020d71c7d427b32b3
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-20 17:32:37 +0100

    arreglo ejercicio 16 operadores

--- 25-02-17_Operadores/Ejercicios/script/Ej16.js
+++ 25-02-17_Operadores/Ejercicios/script/Ej16.js
@@ -9,11 +9,8 @@ Devuelva el array modificado. */
 let nums = [2, 3, 9, 3, -5, -6, 1, 78, -56, 100];
 
 const delnegatives = (nums) => {
-  const nums2 = [];
-  nums.forEach((el) => {
-    el < 0 ? nums2.push(0) : nums2.push(el);
-  });
-  return nums2
+  nums = nums.map((el) => el < 0 ? (le = 0) : el);
+  return nums;
 };
 
 nums = delnegatives(nums);
