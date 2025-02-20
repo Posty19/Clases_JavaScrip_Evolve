6b4c6feaf9b6253fbb8e0b7f38e0dfb54c72a88e
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-20 16:32:34 +0100

    primer ejercicio strings

---
+++ 25-02-18_Strings/Ejercicios/script/Ej1.js
@@ -0,0 +1,9 @@
+/**1️⃣ Obtener iniciales de una lista de nombres
+
+Dado un array con varios nombres completos, crear una función que devuelva un nuevo array con las iniciales de cada nombre en mayúsculas. */
+
+const nombres = ["Dniel", "Jaime", "lucia", "ana", "pedro"];
+
+const iniciales = (names) => names.map((el) => el[0].toUpperCase());
+
+console.log(iniciales(nombres));
