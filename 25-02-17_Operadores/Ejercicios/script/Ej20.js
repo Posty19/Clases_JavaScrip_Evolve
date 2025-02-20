463ef9628be28086e5df96f94da06afe0715b768
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-20 17:45:33 +0100

    ejercicio 20 operadores

--- 25-02-17_Operadores/Ejercicios/script/Ej20.js
+++ 25-02-17_Operadores/Ejercicios/script/Ej20.js
@@ -5,3 +5,16 @@ Dado un array de números, crea un objeto donde:
 Las claves sean los números únicos del array.
 
 Los valores sean la cantidad de veces que aparecen en el array. */
+
+const nums = [];
+
+const contador = nums =>{
+    const result = {};
+
+    for (let i in nums) {
+        let cont = nums[i];
+        result[cont]?++result[cont]:result[cont]=1 
+    }
+    return result
+}
+console.log(contador(nums));
