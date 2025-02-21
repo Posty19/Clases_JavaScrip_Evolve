18b606b2abfe9bc5aef0b25036d52b2d541018da
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-21 08:38:17 +0100

    teoria Number y Math 20/02/25

---
+++ 25-02-20_Numbers_Math/Teoria/script/script.js
@@ -0,0 +1,23 @@
+//Metodos de number
+
+let num = 55.356895;
+
+console.log(num.toString());
+
+console.log(num.toFixed(2));
+
+console.log(num.toExponential(2));
+
+//Metodos de Math
+
+console.log(Math.abs(num));//valor absoluto
+console.log(Math.ceil(num));//redondeo arriba
+console.log(Math.floor(num));//redondeo abajo
+console.log(Math.round(num));//redondeo cercano
+console.log(Math.max(...nums));//mayor valor
+console.log(Math.min(...nums));//menor valor
+console.log(Math.pow(3,2));//baseêxponente
+console.log(Math.sqrt(25));//raiz cuadrada
+console.log(Math.random()*10);//nº aleatorio
+console.log(Math.floor((Math.random()*10)+1));
+console.log(Math.trunc(num));//pasa de los decimales
