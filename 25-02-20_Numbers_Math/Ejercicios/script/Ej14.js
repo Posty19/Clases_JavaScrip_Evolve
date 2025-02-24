506d6e8f413adb8f6ec48110ac4ad4681a4087ad
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-24 16:55:23 +0100

    Ejercicios 15 y 15 de NUmetos y Math

--- 25-02-20_Numbers_Math/Ejercicios/script/Ej14.js
+++ 25-02-20_Numbers_Math/Ejercicios/script/Ej14.js
@@ -3,3 +3,14 @@
 Escribe una función que genere n números aleatorios entre 0 y 100. Cada número generado debe ser redondeado al entero más cercano utilizando Math.round(). Además, la función debe almacenar cada número junto con su versión antes de ser redondeado en un array de objetos con la siguiente estructura: */
 
 //jsCopiarEditar{ original: <número sin redondear>, redondeado: <número redondeado> }
+
+const randNums = n =>{
+    const nums = [];
+    for(let i=0;i<n;i++){
+        const num = Math.random()*100;
+        nums.push({original: num, redondeado: Math.round(num)});
+    }
+    return nums;
+}
+
+console.log(randNums(10));
--- 25-02-20_Numbers_Math/Ejercicios/script/Ej15.js
+++ 25-02-20_Numbers_Math/Ejercicios/script/Ej15.js
@@ -11,3 +11,17 @@ exponencial: El número representado en notación exponencial con dos decimales.
 redondeado: El número redondeado al entero más cercano.
 
 La función debe devolver un array con estos objetos. */
+
+const strNums = ["123.23", "56.56", "96.89", "56.1", "456"];
+
+const objNums = (numsArr) =>
+  numsArr.map((el) => {
+    return {
+      original: el,
+      numero: parseInt(el),
+      exceponencial: parseInt(el).toExponential(2),
+      redondeado: Math.round(parseInt(el)),
+    };
+  });
+
+  console.log(objNums(strNums));
