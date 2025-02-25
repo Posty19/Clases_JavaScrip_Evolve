5ef6b4cccaf00907292c83ef45027b324c6cc126
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-25 18:57:10 +0100

    Correccion ejercicios number y math

--- 25-02-20_Numbers_Math/Ejercicios/script/EJ2.js
+++ 25-02-20_Numbers_Math/Ejercicios/script/EJ2.js
@@ -2,12 +2,28 @@
 
 Escribe una función que tome un array de números decimales y retorne un nuevo array donde cada número haya sido redondeado a exactamente tres decimales utilizando el método toFixed(). Sin embargo, antes de retornar el resultado, la función debe verificar que todos los elementos del array original sean de tipo number y, en caso de encontrar un valor no numérico, debe lanzar un error con un mensaje adecuado. */
 
-const nums = [1.13587,2.325468,56.6845,'margaritas',68.74896,897.456,9.45456,31.967484,8.4856];
+const nums = [
+  1.13587,
+  2.325468,
+  56.6845,
+  "margaritas",
+  68.74896,
+  897.456,
+  9.45456,
+  31.967484,
+  8.4856,
+];
 
-const numsDec = nums => nums.filter(el=>{
-    if(typeof el !== 'number'){
-       console.log(`el elemento ${el} no es un numero`);
-    }else return el.toFixed(3);
-});
+const numsDec = (nums) => {
+  const numsFin = [];
+  nums.forEach((el) => {
+    if (typeof el === "number") {
+      numsFin.push(el.toFixed(3));
+    } else {
+      console.log(`el elemento ${el} no es un numero`);
+    }
+  });
+  return numsFin;
+};
 
 console.log(numsDec(nums));
--- 25-02-20_Numbers_Math/Ejercicios/script/Ej7.js
+++ 25-02-20_Numbers_Math/Ejercicios/script/Ej7.js
@@ -2,24 +2,19 @@
 
 Desarrolla una función que tome un array de números y los redondee al entero más cercano utilizando Math.round(). Sin embargo, si el número tiene exactamente .5 como decimal, en lugar de redondearlo con la regla estándar, la función debe redondearlo hacia el número par más cercano. */
 
-const nums = [
-  1.3 ,
-  1.50 ,
-  -36.45 ,
-  -1.5 ,
-  7.89 ,
-  5
-];
+const nums = [1.3, 1.5, -36.45, -1.5, 7.89, 5];
 
-const redondeo = (nums) =>{
- const n2 = [];
- nums.forEach(el=>{
-  const numStr = el.toString().split('.');
-   if(numStr[1]==5){
-    parseInt(numStr[0]%2)===0 ? n2.push(parseInt(numStr[0])):n2.push(parseInt(numStr[0])+1)
-   }else n2.push(Math.round(el));
- })
- return n2;
-}
+const redondeo = (nums) => {
+  const n2 = [];
+  nums.forEach((el) => {
+    console.log(el % 1);
+    if (el % 1 === 0.5) {
+      el % 2 === 0 ? n2.push(Math.floor(el)) : n2.push(Math.ceil(el));
+    } else if (el % 1 == -0.5) {
+      el % 2 === 0 ? n2.push(Math.ceil(el)) : n2.push(Math.floor(el));
+    } else n2.push(Math.round(el));
+  });
+  return n2;
+};
 
 console.log(redondeo(nums));
