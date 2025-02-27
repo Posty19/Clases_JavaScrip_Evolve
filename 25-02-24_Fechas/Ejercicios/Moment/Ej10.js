46192795a6cd6bb76826e9a531a69ccf3e910a65
Daniel Postigo <danielpostigo96@gmail.com>
2025-02-27 19:39:44 +0100

    retoques al Ej10 de moment

--- 25-02-24_Fechas/Ejercicios/Moment/Ej10.js
+++ 25-02-24_Fechas/Ejercicios/Moment/Ej10.js
@@ -4,7 +4,8 @@ Crea dos fechas: una para el 1 de enero de 2020 y otra para el 15 de marzo de 20
 
 const moment = require('moment')
 
-const date1 = moment("01-01-2020", "DD-MM-YYYY");
-const date2 = moment("15-03-2025", "DD-MM-YYYY");
-const diference = moment(date2.diff(date1));
+const date1 = moment("01-01-1920", "DD-MM-YYYY");
+const date2 = moment("15-03-1925", "DD-MM-YYYY");
+                        //agrego Math.abs() para que independientemente del orden de las fechas la diferencia sea en milisegundos positivos
+const diference = moment(Math.abs(date2.diff(date1)));
 console.log(`${diference.year()-1970} Años, ${diference.month()} Meses y ${diference.date()} Dias`);
