39e10395472b8c48de27d0733aeadfff1db489bf
Daniel Postigo <danielpostigo96@gmail.com>
2025-03-05 09:23:14 +0100

    terminado el javaScript de ejercicio de DOM avanzado

--- 25-03-03_DOM_Avanzado/Ejercicios/script/script.js
+++ 25-03-03_DOM_Avanzado/Ejercicios/script/script.js
@@ -1,89 +1,108 @@
 //*******************************************Creacion de nuevos elementos
-document.getElementById('btnCrear').addEventListener('click',e=>{
-    //cantidad de parrafos para numerar el elemento
-    const pLength = document.querySelectorAll('#contenedorNuevos p.elemento').length;
-    //creacion del nodo texto
-    const textCreation = document.createTextNode(`Elemento creado con numero: ${pLength+1}`);
-    //creacion del parrafo
-    const pElement = document.createElement('p');
-    //asignacion de la clase elemento
-    pElement.setAttribute('class', 'elemento');
-    //añadir el texto al parrafo
-    pElement.append(textCreation);
-    //añadir el parrafo al contenedor
-    document.getElementById('contenedorNuevos').appendChild(pElement);
-})
+document.getElementById("btnCrear").addEventListener("click", (e) => {
+  //cantidad de parrafos para numerar el elemento
+  const pLength = document.querySelectorAll(
+    "#contenedorNuevos p.elemento"
+  ).length;
+  //creacion del nodo texto
+  const textCreation = document.createTextNode(
+    `Elemento creado con numero: ${pLength + 1}`
+  );
+  //creacion del parrafo
+  const pElement = document.createElement("p");
+  //asignacion de la clase elemento
+  pElement.setAttribute("class", "elemento");
+  //añadir el texto al parrafo
+  pElement.append(textCreation);
+  //añadir el parrafo al contenedor
+  document.getElementById("contenedorNuevos").appendChild(pElement);
+});
 
 //*******************************************Sección de inserción posicional
 
 // insertar antes
-document.getElementById('btnInsertarAntes').addEventListener('click',e=>{
-    //tomar el elemento referencia
-    const reference=document.getElementById('elementoReferencia')
-    //creacion del nodo texto
-    const textCreation = document.createTextNode(`Elemento creado antes del elemento de referencia`);
-    //creacion del parrafo
-    const pElement = document.createElement('p');
-    //asignacion de la clase elemento
-    pElement.setAttribute('class', 'elemento');
-    //añadir el texto al parrafo
-    pElement.append(textCreation);
-    //añadir el parrafo al contenedor
-    document.getElementById('contenedorInserciones').insertBefore(pElement,reference);
-})
+document.getElementById("btnInsertarAntes").addEventListener("click", (e) => {
+  //tomar el elemento referencia
+  const reference = document.getElementById("elementoReferencia");
+  //creacion del nodo texto
+  const textCreation = document.createTextNode(
+    `Elemento creado antes del elemento de referencia`
+  );
+  //creacion del parrafo
+  const pElement = document.createElement("p");
+  //asignacion de la clase elemento
+  pElement.setAttribute("class", "elemento");
+  //añadir el texto al parrafo
+  pElement.append(textCreation);
+  //añadir el parrafo al contenedor
+  document
+    .getElementById("contenedorInserciones")
+    .insertBefore(pElement, reference);
+});
 
 // insertar despues
-document.getElementById('btnInsertarDespues').addEventListener('click',e=>{
-    //tomar el elemento referencia
-    const reference=document.getElementById('elementoReferencia')
-    //creacion del nodo texto
-    const textCreation = document.createTextNode(`Elemento creado despues del elemento de referencia`);
-    //creacion del parrafo
-    const pElement = document.createElement('p');
-    //asignacion de la clase elemento
-    pElement.setAttribute('class', 'elemento');
-    //añadir el texto al parrafo
-    pElement.append(textCreation);
-    //añadir el parrafo al contenedor
-    reference.insertAdjacentElement('afterend',pElement);
-})
+document.getElementById("btnInsertarDespues").addEventListener("click", (e) => {
+  //tomar el elemento referencia
+  const reference = document.getElementById("elementoReferencia");
+  //creacion del nodo texto
+  const textCreation = document.createTextNode(
+    `Elemento creado despues del elemento de referencia`
+  );
+  //creacion del parrafo
+  const pElement = document.createElement("p");
+  //asignacion de la clase elemento
+  pElement.setAttribute("class", "elemento");
+  //añadir el texto al parrafo
+  pElement.append(textCreation);
+  //añadir el parrafo al contenedor
+  reference.insertAdjacentElement("afterend", pElement);
+});
 
 //insertar al inicio
-document.getElementById('btnInsertarInicio').addEventListener('click',e=>{
-    //creacion del nodo texto
-    const textCreation = document.createTextNode(`Elemento creado al inicio`);
-    //creacion del parrafo
-    const pElement = document.createElement('p');
-    //asignacion de la clase elemento
-    pElement.setAttribute('class', 'elemento');
-    //añadir el texto al parrafo
-    pElement.append(textCreation);
-    //añadir el parrafo al contenedor
-    document.getElementById('contenedorInserciones').prepend(pElement);
-})
+document.getElementById("btnInsertarInicio").addEventListener("click", (e) => {
+  //creacion del nodo texto
+  const textCreation = document.createTextNode(`Elemento creado al inicio`);
+  //creacion del parrafo
+  const pElement = document.createElement("p");
+  //asignacion de la clase elemento
+  pElement.setAttribute("class", "elemento");
+  //añadir el texto al parrafo
+  pElement.append(textCreation);
+  //añadir el parrafo al contenedor
+  document.getElementById("contenedorInserciones").prepend(pElement);
+});
 
 //insertar al final
-document.getElementById('btnInsertarFinal').addEventListener('click',e=>{
-    //creacion del nodo texto
-    const textCreation = document.createTextNode(`Elemento creado al final`);
-    //creacion del parrafo
-    const pElement = document.createElement('p');
-    //asignacion de la clase elemento
-    pElement.setAttribute('class', 'elemento');
-    //añadir el texto al parrafo
-    pElement.append(textCreation);
-    //añadir el parrafo al contenedor
-    document.getElementById('contenedorInserciones').append(pElement);
-})
+document.getElementById("btnInsertarFinal").addEventListener("click", (e) => {
+  //creacion del nodo texto
+  const textCreation = document.createTextNode(`Elemento creado al final`);
+  //creacion del parrafo
+  const pElement = document.createElement("p");
+  //asignacion de la clase elemento
+  pElement.setAttribute("class", "elemento");
+  //añadir el texto al parrafo
+  pElement.append(textCreation);
+  //añadir el parrafo al contenedor
+  document.getElementById("contenedorInserciones").append(pElement);
+});
 
 //*******************************************Eliminacion de elementos
 
-document.getElementById('btnEliminar').addEventListener('click',e=>{
-    document.getElementById('contenedorEliminables').lastElementChild.remove()
-})
+document.getElementById("btnEliminar").addEventListener("click", (e) => {
+  const lastElement=document.getElementById("contenedorEliminables").lastElementChild
+  lastElement? lastElement.remove():console.log('no quedan elementos por eliminar');
+});
 
 //*******************************************Reemplazar elemento
+let counter = 0;
+document.getElementById("btnReemplazar").addEventListener("click", (e) => {
+  const newElemnt = document.createElement("p");
+  newElemnt.setAttribute("class", "elemento");
+  const textP = document.createTextNode(
+    `Elemento reemplazado numero: ${++counter}`
+  );
+  newElemnt.append(textP);
+  const replazableElement = document.querySelector("#contenedorReemplazo p");
 
-document.getElementById('btnEliminar').addEventListener('click',e=>{
-    document.getElementById('contenedorEliminables').lastElementChild.remove()
-})
+  replazableElement.replaceWith(newElemnt);
+});
