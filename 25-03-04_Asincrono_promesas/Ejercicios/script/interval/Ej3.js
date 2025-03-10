014f6b84a22938eb3c279f2c886dad49a2f92bad
Daniel Postigo <danielpostigo96@gmail.com>
2025-03-10 18:11:53 +0100

    correccion de ejercicios interval

--- 25-03-04_Asincrono_promesas/Ejercicios/script/interval/Ej3.js
+++ 25-03-04_Asincrono_promesas/Ejercicios/script/interval/Ej3.js
@@ -5,5 +5,5 @@ Muestra la hora actual en la consola cada segundo. */
 const watch = setInterval(()=>{
     const now = new Date()
     console.log(now);
-    document.getElementById('relog').textContent=`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    document.getElementById('relog').textContent=now.toLocaleTimeString();
 },1000)
